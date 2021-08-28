cbbc7bfbdd232fc95b98a0196e5c2086735f89de
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 13:23:33 +0530

    Added: code to store computation in array

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,6 +5,7 @@ read -p "Enter second number:" b
 read -p "Enter third number:" c
 
 declare -A dictionary
+array=();
 
 result1=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
@@ -16,5 +17,6 @@ dictionary[value2]=$result2
 dictionary[value3]=$result3
 dictionary[value4]=$result4 
 
-echo ${dictionary[@]}
+array+=${dictionary[@]} 
+echo ${array[@]}
 
