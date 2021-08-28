7da1e3f7ad1e058758ae8647dd10df323243dfea
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 13:14:18 +0530

    Added: code to compute a%b+c

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -7,6 +7,8 @@ read -p "Enter third number:" c
 result1=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
+result4=$(( $a % $b / $c ))
 echo $result1
 echo $result2
+echo $result3
 echo $result3
