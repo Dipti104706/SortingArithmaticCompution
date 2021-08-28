bf1cf039664aa33b80be28bda403e484c28be173
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 13:29:52 +0530

    Added: code to sort the array in descending order

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,18 +5,34 @@ read -p "Enter second number:" b
 read -p "Enter third number:" c
 
 declare -A dictionary
-array=();
 
 result1=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
 result4=$(( $a % $b / $c ))
 
-dictionary[value1]=$result1 
-dictionary[value2]=$result2
-dictionary[value3]=$result3
-dictionary[value4]=$result4 
+function descending() {
+	val=("$@")
+	array=();
 
-array+=${dictionary[@]} 
-echo ${array[@]}
-
+	for values in "${val[@]}"
+	do
+		array+=("${values[@]}")
+	done
+	echo "Array values before sorting:: ${array[@]}" 
+	
+	for (( i=0; i<${#array[@]}; i++ ))
+	do 
+		for (( j=$i; j<${#array[@]}; j++ )) 
+		do 
+			if [ ${array[$i]} -lt ${array[$j]} ]
+		 	then
+				temp=${array[$i]} 
+				array[$i]=${array[$j]} 
+				array[$j]=$temp
+			fi
+		done
+	done
+ echo "Array values after sorting:: ${array[@]}"
+}
+echo $(descending "${dict[@]}")
